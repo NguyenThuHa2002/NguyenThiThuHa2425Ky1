923ac5b492f0cc7523c9ccd3d193fa45801f97a2
NguyenThuHa2002 <nthuha674@gmail.com>
2024-10-17 19:54:04 +0700

    Create_Project

---
+++ NguyenThiThuHa231/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiThuHa231.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
